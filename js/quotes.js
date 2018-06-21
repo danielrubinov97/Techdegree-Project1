f72d5432b9d8ab83ebb7b35b8643a403cb736d26
Daniel Rubinov <danielrubinov97@gmail.com>
2018-06-20 21:13:38 -0400

    Finished Project
    Could add more, but have already exceeded the exceeding expectations. :D

---
+++ js/quotes.js
@@ -0,0 +1,38 @@
+var quotes = [
+	{ quote : 'We\'re made of star stuff. We are a way for the cosmos to know itself.',
+	  source : 'Carl Sagan',
+	  citation : 'Cosmos: A Personal Voyage (Episode 1)',
+	  year : 1990,
+	  tags : ['science', 'life', 'nature']
+	},
+	{ quote : 'For me, it is far better to grasp the Universe as it really is than to persist in delusion, however satisfying and reassuring.',
+	  source : 'Carl Sagan',
+	  citation : 'The Demon Haunted World : Science as a Candle in the Dark (Chapter 1)',
+	  year : 1995,
+	  tags : ['science', 'religion', 'Universe', 'satisfaction', 'reassuring']
+	},
+	{ quote : 'The first principle is that you must not fool yourself — and you are the easiest person to fool.',
+	  source : 'Richard Phillip Feynman',
+	  citation : 'Surely You\'re Joking Mr. Feynman! (Page 342)',
+	  year : 1974,
+	  tags : ['science', 'yourself', 'fool', 'truth', 'lies']
+	},
+	{ quote : 'The imagination of nature is far, far greater than the imagination of man.',
+	  source : 'Richard Phillip Feynman',
+	  citation : 'The Value of Science',
+	  year : 1955,
+	  tags : ['nature', 'science', 'imagination', 'humanity', 'humility']
+	},
+	{ quote : 'As I have said so many times, God doesn\'t play dice with the world.',
+	  source : 'Albert Einstein',
+	  citation : 'Einstein and the Poet',
+	  year : 1943,
+	  tags : ['science', 'nature', 'God', 'Quantum Mechanics']
+	},
+	{ quote : 'Once upon a time, people identified the god Neptune as the source of storms at sea. Today we call these storms hurricanes ... The only people who still call hurricanes acts of God are the people who write insurance forms.',
+	  source : 'Neil Degrasse Tyson',
+	  citation : 'Death by Black Hole',
+	  year : 2007,
+	  tags : ['science', 'religion', 'God', 'storms', 'insurance', 'joke']
+	}
+];
--- js/script.js
+++ js/script.js
@@ -1,3 +1,60 @@
+function getRandomNumber(){
+	console.log('Made it!');
+	return Math.floor(Math.random() * 6)
+}
+
+var arrayPreviousRandom = [];
+
+function getRandomQuote() {
+	console.log('Made it!');
+	var randomNumber = getRandomNumber();
+	arrayPreviousRandom.push(randomNumber);
+	console.log(arrayPreviousRandom);
+	while(arrayPreviousRandom[arrayPreviousRandom.length - 2] === arrayPreviousRandom[arrayPreviousRandom.length - 1]){
+		console.log('In the IF');
+		randomNumber = getRandomNumber();
+		arrayPreviousRandom.push(randomNumber);
+	}
+	return quotes[randomNumber]
+}
+
+var nIntervId;
+
+function printQuote() {
+	window.clearTimeout(nIntervId);
+	console.log('Made it!');
+	var returnedQuote = getRandomQuote();
+	var HTML  = '<p class = "quote">';
+	for(var prop in returnedQuote){
+		console.log('FOR IN');
+		if (prop === 'quote'){
+			HTML += returnedQuote[prop] + '</p>';
+		} else if (prop === 'source'){
+			HTML += '<p class = "source">' + returnedQuote[prop];
+		} else if (prop === 'citation'){
+			HTML += '<span class = "citation" >' + returnedQuote[prop] + '</span>';
+		} else if (prop === 'year'){
+			HTML += '<span class = "year">' + returnedQuote[prop] + '</span></p>';
+		} else {
+			HTML += '<p class = "tags">' + returnedQuote[prop].join(', ') + '</p>';
+		}
+	}
+	var backgroundColors = ['black', 'darkblue', 'green', 'darkgrey', 'darkgreen', 'maroon'];
+	var randomNumber = getRandomNumber();
+
+	document.body.style.backgroundColor = backgroundColors[randomNumber];
+	document.getElementById('loadQuote').style.backgroundColor = backgroundColors[randomNumber];
+
+	document.getElementById('quote-box').innerHTML = HTML;
+	timedEvent();
+}
+
+function timedEvent(){
+	nIntervId = window.setTimeout(printQuote, 15000);
+}
+
+timedEvent();
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
